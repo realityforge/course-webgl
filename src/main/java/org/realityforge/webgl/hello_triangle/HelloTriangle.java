00f5d4a54d2da8d6e52586f00538bfd21b472b9c
Peter Donald <peter@realityforge.org>
2019-08-25 09:09:38 +1000

    Add initial application template

---
+++ src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
@@ -0,0 +1,12 @@
+package org.realityforge.webgl.hello_triangle;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class HelloTriangle
+  implements EntryPoint
+{
+  @Override
+  public void onModuleLoad()
+  {
+  }
+}
