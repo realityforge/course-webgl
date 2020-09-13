f3eb06cd6aa4edaa9fc71266433602c51e2da597
Peter Donald <peter@realityforge.org>
2020-09-14 08:38:37 +1000

    Start to get gl canvas context

--- src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
+++ src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
@@ -1,6 +1,9 @@
 package org.realityforge.webgl.hello_triangle;
 
 import com.google.gwt.core.client.EntryPoint;
+import elemental3.Global;
+import elemental3.HTMLCanvasElement;
+import elemental3.WebGL2RenderingContext;
 
 public class HelloTriangle
   implements EntryPoint
@@ -8,5 +11,9 @@ public class HelloTriangle
   @Override
   public void onModuleLoad()
   {
+    final HTMLCanvasElement renderCanvas =
+      (HTMLCanvasElement) Global.globalThis().document().getElementById( "renderCanvas" );
+    assert null != renderCanvas;
+    final WebGL2RenderingContext context = (WebGL2RenderingContext) renderCanvas.getContext( "webgl2" );
   }
 }
