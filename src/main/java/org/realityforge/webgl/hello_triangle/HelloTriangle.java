0b9cafaf81849d3610859452e6e4e7c10d3d80aa
Peter Donald <peter@realityforge.org>
2020-09-14 20:52:49 +1000

    First setup

--- src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
+++ src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
@@ -1,11 +1,13 @@
 package org.realityforge.webgl.hello_triangle;
 
 import com.google.gwt.core.client.EntryPoint;
+import elemental2.core.Float32Array;
 import elemental3.Document;
 import elemental3.Global;
 import elemental3.HTMLCanvasElement;
 import elemental3.HTMLElement;
 import elemental3.WebGL2RenderingContext;
+import elemental3.WebGLBuffer;
 import javax.annotation.Nonnull;
 
 public class HelloTriangle
@@ -16,7 +18,46 @@ public class HelloTriangle
   {
     final HTMLCanvasElement canvas = createCanvas();
     // TODO: This method should have typing tightened up
-    final WebGL2RenderingContext context = (WebGL2RenderingContext) canvas.getContext( "webgl2" );
+    final WebGL2RenderingContext gl = (WebGL2RenderingContext) canvas.getContext( "webgl2" );
+    assert null != gl;
+
+    // Vertex position data for triangle
+    double[] positions = new double[]{
+      // Vertex 1
+      1.0, -1.0, 0.0,
+      // Vertex 1
+      1.0, 1.0, 0.0,
+      // Vertex 1
+      -1.0, -1.0, 0.0
+    };
+
+    // Vertex color data for triangle in RGBA form
+    double[] colors = new double[]{
+      // Red
+      1.0, 0.0, 0.0, 1.0,
+      // Green
+      0.0, 1.0, 0.0, 1.0,
+      // Blue
+      0.0, 0.0, 1.0, 1.0
+    };
+
+    // Create a GPU resource for position data
+    final WebGLBuffer positionBuffer = gl.createBuffer();
+    // Bind a gate between CPU and GPU
+    gl.bindBuffer( WebGL2RenderingContext.ARRAY_BUFFER, positionBuffer );
+
+    // Send data via ARRAY_BUFFER gate and whatever it is bound to which is a buffer in this case
+    // The last parameter is a hint indicating that this data is static and the CPU will not update it often
+    // which means that the GPU can store it close to where it is used without worrying about latency to update
+    gl.bufferData( WebGL2RenderingContext.ARRAY_BUFFER,
+                   new Float32Array( positions ),
+                   WebGL2RenderingContext.STATIC_DRAW );
+
+    final WebGLBuffer colorBuffer = gl.createBuffer();
+    gl.bindBuffer( WebGL2RenderingContext.ARRAY_BUFFER, colorBuffer );
+    gl.bufferData( WebGL2RenderingContext.ARRAY_BUFFER,
+                   new Float32Array( colors ),
+                   WebGL2RenderingContext.STATIC_DRAW );
   }
 
   @Nonnull
