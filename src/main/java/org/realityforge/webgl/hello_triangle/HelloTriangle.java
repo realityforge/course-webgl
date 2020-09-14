13ce164fc59b3298f5fc6230d4a0cf9888c2553c
Peter Donald <peter@realityforge.org>
2020-09-14 23:21:10 +1000

    Fix labels

--- src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
+++ src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
@@ -28,9 +28,9 @@ public class HelloTriangle
     double[] positions = new double[]{
       // Vertex 1
       1.0, -1.0, 0.0,
-      // Vertex 1
-      // Vertex 1
+      // Vertex 2
       0.0, 1.0, 0.0,
+      // Vertex 3
       -1.0, -1.0, 0.0
     };
 
