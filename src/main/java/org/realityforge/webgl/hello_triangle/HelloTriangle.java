24cd0973f411cb1be207a20fbbc2b3d88ec24aec
Peter Donald <peter@realityforge.org>
2020-09-14 20:19:59 +1000

    Create canvas element

--- src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
+++ src/main/java/org/realityforge/webgl/hello_triangle/HelloTriangle.java
@@ -1,9 +1,12 @@
 package org.realityforge.webgl.hello_triangle;
 
 import com.google.gwt.core.client.EntryPoint;
+import elemental3.Document;
 import elemental3.Global;
 import elemental3.HTMLCanvasElement;
+import elemental3.HTMLElement;
 import elemental3.WebGL2RenderingContext;
+import javax.annotation.Nonnull;
 
 public class HelloTriangle
   implements EntryPoint
@@ -11,11 +14,21 @@ public class HelloTriangle
   @Override
   public void onModuleLoad()
   {
-    // TODO: Rather than retrieve this from page maybe create and attach to body?
-    final HTMLCanvasElement renderCanvas =
-      (HTMLCanvasElement) Global.globalThis().document().getElementById( "renderCanvas" );
-    assert null != renderCanvas;
+    final HTMLCanvasElement canvas = createCanvas();
     // TODO: This method should have typing tightened up
-    final WebGL2RenderingContext context = (WebGL2RenderingContext) renderCanvas.getContext( "webgl2" );
+    final WebGL2RenderingContext context = (WebGL2RenderingContext) canvas.getContext( "webgl2" );
+  }
+
+  @Nonnull
+  private HTMLCanvasElement createCanvas()
+  {
+    final Document document = Global.globalThis().document();
+    final HTMLCanvasElement canvas = (HTMLCanvasElement) document.createElement( "canvas" );
+    canvas.width = 1024;
+    canvas.height = 786;
+    final HTMLElement body = document.body;
+    assert null != body;
+    body.appendChild( canvas );
+    return canvas;
   }
 }
