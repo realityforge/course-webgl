32be882e3d089ba8dd27fcdb54b0b50ad6baa36e
Peter Donald <peter@realityforge.org>
2020-09-16 19:51:01 +1000

    Define mesh factory for cube

---
+++ src/main/java/org/realityforge/webgl/cube/CubeTemplate.java
@@ -0,0 +1,109 @@
+package org.realityforge.webgl.cube;
+
+import elemental2.core.Float32Array;
+import elemental3.WebGL2RenderingContext;
+import javax.annotation.Nonnull;
+
+final class CubeTemplate
+{
+  // Vertices position data
+  private static final double[] POSITIONS = new double[]
+    {
+      -0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, 0.5, -0.5,
+      0.5, 0.5, -0.5,
+      -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+
+      -0.5, -0.5, 0.5,
+      0.5, -0.5, 0.5,
+      0.5, 0.5, 0.5,
+      0.5, 0.5, 0.5,
+      -0.5, 0.5, 0.5,
+      -0.5, -0.5, 0.5,
+
+      -0.5, 0.5, 0.5,
+      -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+      -0.5, -0.5, -0.5,
+      -0.5, -0.5, 0.5,
+      -0.5, 0.5, 0.5,
+
+      0.5, 0.5, 0.5,
+      0.5, 0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, 0.5,
+      0.5, 0.5, 0.5,
+
+      -0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, 0.5,
+      0.5, -0.5, 0.5,
+      -0.5, -0.5, 0.5,
+      -0.5, -0.5, -0.5,
+
+      -0.5, 0.5, -0.5,
+      0.5, 0.5, -0.5,
+      0.5, 0.5, 0.5,
+      0.5, 0.5, 0.5,
+      -0.5, 0.5, 0.5,
+      -0.5, 0.5, -0.5
+    };
+  // Vertices color data in RGBA form
+  private static final double[] COLORS = new double[]
+    {
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0 // Left face
+    };
+
+  private CubeTemplate()
+  {
+  }
+
+  @Nonnull
+  public static Mesh create( @Nonnull final WebGL2RenderingContext gl )
+  {
+    return new Mesh( gl, new Float32Array( POSITIONS ), new Float32Array( COLORS ) );
+  }
+}
