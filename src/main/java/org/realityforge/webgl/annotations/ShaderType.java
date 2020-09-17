6045b0f73636aac9c74e938becd857dc501fa726
Peter Donald <peter@realityforge.org>
2020-09-18 09:12:49 +1000

    Add TODO

--- src/main/java/org/realityforge/webgl/annotations/ShaderType.java
+++ src/main/java/org/realityforge/webgl/annotations/ShaderType.java
@@ -3,6 +3,7 @@ package org.realityforge.webgl.annotations;
 import elemental3.WebGL2RenderingContext;
 import org.intellij.lang.annotations.MagicConstant;
 
+// TODO: This should be generated as part of WebGL2RenderingContext
 @MagicConstant( intValues = { WebGL2RenderingContext.VERTEX_SHADER, WebGL2RenderingContext.FRAGMENT_SHADER } )
 public @interface ShaderType
 {
