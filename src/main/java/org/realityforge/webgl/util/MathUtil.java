f5aad2679747e71d789bbc429f35a4e37ba84e5b
Peter Donald <peter@realityforge.org>
2020-09-18 10:10:32 +1000

    Add some additional support methods

--- src/main/java/org/realityforge/webgl/util/MathUtil.java
+++ src/main/java/org/realityforge/webgl/util/MathUtil.java
@@ -3,11 +3,24 @@ package org.realityforge.webgl.util;
 import elemental2.core.Float32Array;
 import javax.annotation.Nonnull;
 import org.joml.Matrix4d;
+import org.joml.Vector3d;
+import org.joml.Vector4d;
 
 public final class MathUtil
 {
   private MathUtil()
   {
+  }
+
+  @Nonnull
+  public static Float32Array toFloat32Array( @Nonnull final Vector4d input )
+  {
+    return new Float32Array( new double[]{ input.x, input.y, input.z, input.w } );
+  }
+  @Nonnull
+  public static Float32Array toFloat32Array( @Nonnull final Vector3d input )
+  {
+    return new Float32Array( new double[]{ input.x, input.y, input.z } );
   }
 
   @Nonnull
