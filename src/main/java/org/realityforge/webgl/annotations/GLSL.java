f7810f128fb3b71a5012bd8a3e2670e3ad75fd84
Peter Donald <peter@realityforge.org>
2020-09-20 14:15:52 +1000

    Add TODO

--- src/main/java/org/realityforge/webgl/annotations/GLSL.java
+++ src/main/java/org/realityforge/webgl/annotations/GLSL.java
@@ -2,6 +2,7 @@ package org.realityforge.webgl.annotations;
 
 import org.intellij.lang.annotations.Language;
 
+// TODO: Figure out a way for this to be applied by webtack...
 @Language( "GLSL" )
 public @interface GLSL
 {
