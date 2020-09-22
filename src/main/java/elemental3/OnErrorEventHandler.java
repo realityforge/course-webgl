67baf10b155e1be6cf29250c52b794970b145b6b
Peter Donald <peter@realityforge.org>
2020-09-22 14:05:59 +1000

    Regenerate

--- src/main/java/elemental3/OnErrorEventHandler.java
+++ src/main/java/elemental3/OnErrorEventHandler.java
@@ -5,13 +5,11 @@ import javax.annotation.Generated;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import jsinterop.annotations.JsFunction;
-import jsinterop.base.Any;
 
 @Generated("org.realityforge.webtack")
 @JsFunction
 @FunctionalInterface
 public interface OnErrorEventHandler {
-  @Nullable
-  Any onInvoke(@Nonnull EventOrStringUnion event, @Nonnull String source, int lineno, int colno,
+  void onInvoke(@Nonnull EventOrStringUnion event, @Nonnull String source, int lineno, int colno,
       @DoNotAutobox @Nullable Object error);
 }
