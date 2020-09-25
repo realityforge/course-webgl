03a44e3253c754951345292a8434ec4735f3498b
Peter Donald <peter@realityforge.org>
2020-09-26 07:49:00 +1000

    Fix assertion to check for correct invalid value

--- src/main/java/org/realityforge/webgl/util/BufferAttributeBinding.java
+++ src/main/java/org/realityforge/webgl/util/BufferAttributeBinding.java
@@ -25,7 +25,7 @@ public final class BufferAttributeBinding
     _attribute = Objects.requireNonNull( attribute );
     _location = gl.getAttribLocation( program, name );
     //TODO: We should improve the handling of this error
-    assert WebGL2RenderingContext.INVALID_ENUM != _location;
+    assert WebGL2RenderingContext.INVALID_INDEX != _location;
   }
 
   @Nonnull
