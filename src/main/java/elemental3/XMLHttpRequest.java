13c3d798a416223efc9c493c26305fefcbdb4c70
Peter Donald <peter@realityforge.org>
2020-10-13 10:54:33 +1100

    Regenerate

--- src/main/java/elemental3/XMLHttpRequest.java
+++ src/main/java/elemental3/XMLHttpRequest.java
@@ -95,6 +95,7 @@ public class XMLHttpRequest extends XMLHttpRequestEventTarget {
   @JsProperty(
       name = "readyState"
   )
+  @XMLHttpRequestReadyStateType
   public native int readyState();
 
   /**
