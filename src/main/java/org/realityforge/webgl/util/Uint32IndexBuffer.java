18e337015a80cacf17e0d8a5164ef24900acda35
Peter Donald <peter@realityforge.org>
2020-10-27 22:55:57 +1100

    Add Uin32IndexBuffer

---
+++ src/main/java/org/realityforge/webgl/util/Uint32IndexBuffer.java
@@ -0,0 +1,29 @@
+package org.realityforge.webgl.util;
+
+import elemental2.core.Uint32Array;
+import elemental3.gl.UsageType;
+import elemental3.gl.WebGL2RenderingContext;
+import elemental3.gl.WebGLBuffer;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public final class Uint32IndexBuffer
+  extends IndexBuffer<Uint32Array>
+{
+  public Uint32IndexBuffer( @Nonnull final Uint32Array data )
+  {
+    this( data, WebGL2RenderingContext.STATIC_DRAW );
+  }
+
+  public Uint32IndexBuffer( @Nonnull final Uint32Array data, @UsageType final int usage )
+  {
+    this( data, usage, null );
+  }
+
+  public Uint32IndexBuffer( @Nonnull final Uint32Array data,
+                            @UsageType final int usage,
+                            @Nullable final WebGLBuffer buffer )
+  {
+    super( data, usage, WebGL2RenderingContext.UNSIGNED_INT, buffer );
+  }
+}
