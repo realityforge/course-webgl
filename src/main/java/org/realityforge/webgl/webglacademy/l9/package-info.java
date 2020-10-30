672ff36b921c6dab459553c48e0fc3df227b70a0
Peter Donald <peter@realityforge.org>
2020-10-31 08:22:48 +1100

    Run through lesson 9

---
+++ src/main/java/org/realityforge/webgl/webglacademy/l9/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Experiment based on http://www.webglacademy.com/courses.php?courses=9_12_24_13#9
+ * which is also where the assets were retrieved from.
+ */
+package org.realityforge.webgl.webglacademy.l9;
