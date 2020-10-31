9556056be295bdebe37d324691fec4e5cbbd0caa
Peter Donald <peter@realityforge.org>
2020-10-31 22:53:26 +1100

    Regenerate

--- src/main/java/elemental3/gl/WebGL2RenderingContext.java
+++ src/main/java/elemental3/gl/WebGL2RenderingContext.java
@@ -2089,8 +2089,9 @@ public class WebGL2RenderingContext {
    *
    * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/framebufferTexture2D">WebGLRenderingContext.framebufferTexture2D - MDN</a>
    */
-  public native void framebufferTexture2D(@FramebufferTargetType int target, int attachment,
-      @Texture2DSurfaceTargetType int textarget, @Nullable WebGLTexture texture, int level);
+  public native void framebufferTexture2D(@FramebufferTargetType int target,
+      @FramebufferAttachment int attachment, @Texture2DSurfaceTargetType int textarget,
+      @Nullable WebGLTexture texture, @FramebufferMipmapLevel int level);
 
   /**
    * The WebGLRenderingContext.frontFace() method of the WebGL API specifies whether polygons are front- or back-facing by setting a winding orientation.
