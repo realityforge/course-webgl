2ca3d999c5d613b7a557170dce31ccf0580108ac
Peter Donald <peter@realityforge.org>
2020-10-31 21:51:06 +1100

    Initialize base code for lesson 10

---
+++ src/main/java/org/realityforge/webgl/webglacademy/l10/Mesh.java
@@ -0,0 +1,57 @@
+package org.realityforge.webgl.webglacademy.l10;
+
+import elemental3.gl.WebGL2RenderingContext;
+import elemental3.gl.WebGLProgram;
+import elemental3.gl.WebGLTexture;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.joml.Matrix4d;
+import org.realityforge.webgl.util.AppState;
+import org.realityforge.webgl.util.GL;
+import org.realityforge.webgl.util.Geometry;
+import org.realityforge.webgl.util.MathUtil;
+
+final class Mesh
+{
+  @Nonnull
+  private final Geometry _geometry;
+  @Nonnull
+  private final Material _material;
+
+  Mesh( @Nonnull final Geometry geometry, @Nonnull final Material material )
+  {
+    _geometry = Objects.requireNonNull( geometry );
+    _material = Objects.requireNonNull( material );
+  }
+
+  void render( @Nonnull final Matrix4d modelMatrix,
+               @Nonnull final Matrix4d viewMatrix,
+               @Nonnull final Matrix4d projectionMatrix,
+               @Nonnull final WebGLTexture texture )
+  {
+    final AppState appState = AppState.get();
+    appState.useProgram( _material.getProgram() );
+
+    final WebGL2RenderingContext gl = appState.gl();
+    gl.uniformMatrix4fv( _material.getModelMatrixLocation(), false, MathUtil.toFloat32Array( modelMatrix ) );
+    gl.uniformMatrix4fv( _material.getViewMatrixLocation(), false, MathUtil.toFloat32Array( viewMatrix ) );
+    gl.uniformMatrix4fv( _material.getProjectionMatrixLocation(), false, MathUtil.toFloat32Array( projectionMatrix ) );
+    gl.activeTexture( WebGL2RenderingContext.TEXTURE0 );
+    gl.bindTexture( WebGL2RenderingContext.TEXTURE_2D, texture );
+    gl.uniform1i( _material.getTextureLocation(), 0 );
+
+    _geometry.draw();
+  }
+
+  void sendToGpu( @Nonnull final WebGL2RenderingContext gl )
+  {
+    final AppState appState = AppState.get();
+    final WebGLProgram program = _material.getProgram();
+    appState.useProgram( program );
+
+    _geometry.getAttribute( 0 ).setLocation( GL.getAttribLocation( gl, program, "position" ) );
+    _geometry.getAttribute( 1 ).setLocation( GL.getAttribLocation( gl, program, "normal" ) );
+    _geometry.getAttribute( 2 ).setLocation( GL.getAttribLocation( gl, program, "uv" ) );
+    _geometry.uploadToCpu();
+  }
+}
---
+++ src/main/java/org/realityforge/webgl/webglacademy/l10/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Experiment based on http://www.webglacademy.com/courses.php?courses=0_1_20_2_3_4_23_5_6_7_10#10
+ */
+package org.realityforge.webgl.webglacademy.l10;
