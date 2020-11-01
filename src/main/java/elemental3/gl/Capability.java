1b04655b341806755bca19e9dc1286f947b8765d
Peter Donald <peter@realityforge.org>
2020-11-02 09:34:27 +1100

    Regenerate

---
+++ src/main/java/elemental3/gl/Capability.java
@@ -0,0 +1,24 @@
+package elemental3.gl;
+
+import java.lang.annotation.Documented;
+import javax.annotation.Generated;
+import org.intellij.lang.annotations.MagicConstant;
+
+@Generated("org.realityforge.webtack")
+@Documented
+@MagicConstant(
+    intValues = {
+        WebGL2RenderingContext.BLEND,
+        WebGL2RenderingContext.CULL_FACE,
+        WebGL2RenderingContext.DEPTH_TEST,
+        WebGL2RenderingContext.DITHER,
+        WebGL2RenderingContext.POLYGON_OFFSET_FILL,
+        WebGL2RenderingContext.SAMPLE_ALPHA_TO_COVERAGE,
+        WebGL2RenderingContext.SAMPLE_COVERAGE,
+        WebGL2RenderingContext.SCISSOR_TEST,
+        WebGL2RenderingContext.STENCIL_TEST,
+        WebGL2RenderingContext.RASTERIZER_DISCARD
+    }
+)
+public @interface Capability {
+}
