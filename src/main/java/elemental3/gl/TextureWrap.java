b1716f8757009629165c82188ea8518209dd8eb5
Peter Donald <peter@realityforge.org>
2020-11-02 20:12:24 +1100

    Use wrap enum

---
+++ src/main/java/elemental3/gl/TextureWrap.java
@@ -0,0 +1,17 @@
+package elemental3.gl;
+
+import java.lang.annotation.Documented;
+import javax.annotation.Generated;
+import org.intellij.lang.annotations.MagicConstant;
+
+@Generated("org.realityforge.webtack")
+@Documented
+@MagicConstant(
+    intValues = {
+        WebGL2RenderingContext.REPEAT,
+        WebGL2RenderingContext.CLAMP_TO_EDGE,
+        WebGL2RenderingContext.MIRRORED_REPEAT
+    }
+)
+public @interface TextureWrap {
+}
