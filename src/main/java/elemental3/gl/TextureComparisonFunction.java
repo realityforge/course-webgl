bab50bd373b6fca04a605055d1eb02fd6c813cea
Peter Donald <peter@realityforge.org>
2020-11-07 08:28:58 +1100

    Regenerate

---
+++ src/main/java/elemental3/gl/TextureComparisonFunction.java
@@ -0,0 +1,22 @@
+package elemental3.gl;
+
+import java.lang.annotation.Documented;
+import javax.annotation.Generated;
+import org.intellij.lang.annotations.MagicConstant;
+
+@Generated("org.realityforge.webtack")
+@Documented
+@MagicConstant(
+    intValues = {
+        WebGL2RenderingContext.LEQUAL,
+        WebGL2RenderingContext.GEQUAL,
+        WebGL2RenderingContext.LESS,
+        WebGL2RenderingContext.GREATER,
+        WebGL2RenderingContext.EQUAL,
+        WebGL2RenderingContext.NOTEQUAL,
+        WebGL2RenderingContext.ALWAYS,
+        WebGL2RenderingContext.NEVER
+    }
+)
+public @interface TextureComparisonFunction {
+}
