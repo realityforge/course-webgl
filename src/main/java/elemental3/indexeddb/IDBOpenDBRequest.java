13db304095585d63c10c1d22f7179920cd179d8b
Peter Donald <peter@realityforge.org>
2020-11-08 16:25:13 +1100

    Regenerate

--- src/main/java/elemental3/indexeddb/IDBOpenDBRequest.java
+++ src/main/java/elemental3/indexeddb/IDBOpenDBRequest.java
@@ -1,8 +1,6 @@
 package elemental3.indexeddb;
 
 import elemental3.AddEventListenerOptions;
-import elemental3.EventHandler;
-import elemental3.EventListener;
 import elemental3.EventListenerOptions;
 import javax.annotation.Generated;
 import javax.annotation.Nonnull;
@@ -16,6 +14,8 @@ import jsinterop.base.Js;
  * Also inherits methods from its parents IDBRequest and EventTarget.
  *
  * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest">IDBOpenDBRequest - MDN</a>
+ * @see <a href="https://www.w3.org/TR/IndexedDB/#idbopendbrequest">The definition of 'IDBOpenDBRequest' in Indexed Database API 2.0.</a>
+ * @see <a href="https://w3c.github.io/IndexedDB/#idbopendbrequest">The definition of 'IDBOpenDBRequest' in Indexed Database API Draft.</a>
  */
 @Generated("org.realityforge.webtack")
 @JsType(
@@ -28,6 +28,8 @@ public class IDBOpenDBRequest extends IDBRequest {
    * The onblocked event handler of the IDBOpenDBRequest interface is the event handler for the blocked event. This event is triggered when the upgradeneeded should be triggered because of a version change but the database is still in use (that is, not closed) somewhere, even after the versionchange event was sent.
    *
    * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest/onblocked">IDBOpenDBRequest.onblocked - MDN</a>
+   * @see <a href="https://www.w3.org/TR/IndexedDB/#dom-idbopendbrequest-onblocked">The definition of 'onblocked' in Indexed Database API 2.0.</a>
+   * @see <a href="https://w3c.github.io/IndexedDB/#dom-idbopendbrequest-onblocked">The definition of 'onblocked' in Indexed Database API Draft.</a>
    */
   @Nullable
   public IDBVersionChangeEventHandler onblocked;
@@ -36,9 +38,11 @@ public class IDBOpenDBRequest extends IDBRequest {
    * In the following example you can see the onupgradeneeded handler being used to update the database structure if a database with a higher version number is loaded. For a full working example, see our To-do Notifications app (view example live.)
    *
    * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/IDBOpenDBRequest/onupgradeneeded">IDBOpenDBRequest.onupgradeneeded - MDN</a>
+   * @see <a href="https://www.w3.org/TR/IndexedDB/#dom-idbopendbrequest-onupgradeneeded">The definition of 'onupgradeneeded' in Indexed Database API 2.0.</a>
+   * @see <a href="https://w3c.github.io/IndexedDB/#dom-idbopendbrequest-onupgradeneeded">The definition of 'onupgradeneeded' in Indexed Database API Draft.</a>
    */
   @Nullable
-  public EventHandler onupgradeneeded;
+  public IDBVersionChangeEventHandler onupgradeneeded;
 
   protected IDBOpenDBRequest() {
   }
@@ -78,36 +82,39 @@ public class IDBOpenDBRequest extends IDBRequest {
   }
 
   @JsOverlay
-  public final void addUpgradeneededListener(@Nonnull final EventListener callback,
+  public final void addUpgradeneededListener(@Nonnull final IDBVersionChangeEventListener callback,
       @Nonnull final AddEventListenerOptions options) {
     addEventListener( "upgradeneeded", Js.cast( callback ), options );
   }
 
   @JsOverlay
-  public final void addUpgradeneededListener(@Nonnull final EventListener callback,
+  public final void addUpgradeneededListener(@Nonnull final IDBVersionChangeEventListener callback,
       final boolean useCapture) {
     addEventListener( "upgradeneeded", Js.cast( callback ), useCapture );
   }
 
   @JsOverlay
-  public final void addUpgradeneededListener(@Nonnull final EventListener callback) {
+  public final void addUpgradeneededListener(
+      @Nonnull final IDBVersionChangeEventListener callback) {
     addEventListener( "upgradeneeded", Js.cast( callback ) );
   }
 
   @JsOverlay
-  public final void removeUpgradeneededListener(@Nonnull final EventListener callback,
+  public final void removeUpgradeneededListener(
+      @Nonnull final IDBVersionChangeEventListener callback,
       @Nonnull final EventListenerOptions options) {
     removeEventListener( "upgradeneeded", Js.cast( callback ), options );
   }
 
   @JsOverlay
-  public final void removeUpgradeneededListener(@Nonnull final EventListener callback,
-      final boolean useCapture) {
+  public final void removeUpgradeneededListener(
+      @Nonnull final IDBVersionChangeEventListener callback, final boolean useCapture) {
     removeEventListener( "upgradeneeded", Js.cast( callback ), useCapture );
   }
 
   @JsOverlay
-  public final void removeUpgradeneededListener(@Nonnull final EventListener callback) {
+  public final void removeUpgradeneededListener(
+      @Nonnull final IDBVersionChangeEventListener callback) {
     removeEventListener( "upgradeneeded", Js.cast( callback ) );
   }
 }
