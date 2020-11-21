ef952ed01742fc1dbc1d23d107fa668682a45a77
Peter Donald <peter@realityforge.org>
2020-11-22 08:21:40 +1100

    Regenerate

--- src/main/java/elemental3/MathNamespace.java
+++ src/main/java/elemental3/MathNamespace.java
@@ -5,6 +5,11 @@ import javax.annotation.Nonnull;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
+/**
+ * Math is a built-in object that has properties and methods for mathematical constants and functions. It&rsquo;s not a function object.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math">Math - MDN</a>
+ */
 @Generated("org.realityforge.webtack")
 @JsType(
     isNative = true,
@@ -15,7 +20,17 @@ public final class MathNamespace {
   private MathNamespace() {
   }
 
+  /**
+   * The Math.abs() function returns the absolute value of a number
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs">Math.abs - MDN</a>
+   */
   public native double abs(@Nonnull String x);
 
+  /**
+   * The Math.abs() function returns the absolute value of a number
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs">Math.abs - MDN</a>
+   */
   public native double abs(double x);
 }
