40aa4b2efdc72fd8acc47a04d448352f3a6cec54
Peter Donald <peter@realityforge.org>
2021-01-24 23:33:24 +1100

    Rename unused class

--- src/main/java/org/realityforge/webgl/util/Float2Uniform.java
+++ src/main/java/org/realityforge/webgl/util/Float2Uniform.java
@@ -4,17 +4,17 @@ import elemental3.gl.WebGL2RenderingContext;
 import elemental3.gl.WebGLProgram;
 import javax.annotation.Nonnull;
 
-public final class Vec2FUniform
+public final class Float2Uniform
   extends Uniform
 {
   private float _x;
   private float _y;
 
-  public Vec2FUniform( @Nonnull final WebGL2RenderingContext gl,
-                       @Nonnull final WebGLProgram program,
-                       @Nonnull final String name,
-                       final float x,
-                       final float y )
+  public Float2Uniform( @Nonnull final WebGL2RenderingContext gl,
+                        @Nonnull final WebGLProgram program,
+                        @Nonnull final String name,
+                        final float x,
+                        final float y )
   {
     super( gl, program, name );
     _x = x;
