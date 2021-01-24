66b31575f5af0082bf816a707c3ac1fe27e1cc97
Peter Donald <peter@realityforge.org>
2021-01-24 16:26:55 +1100

    Extract a common superclass for buffers

--- src/main/java/org/realityforge/webgl/util/AttributeBuffer.java
+++ src/main/java/org/realityforge/webgl/util/AttributeBuffer.java
@@ -3,18 +3,13 @@ package org.realityforge.webgl.util;
 import elemental3.core.ArrayBufferView;
 import elemental3.gl.UsageType;
 import elemental3.gl.WebGL2RenderingContext;
-import elemental3.gl.WebGLBuffer;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import org.realityforge.webgl.util.v2.Accessor;
 
 public final class AttributeBuffer
-  extends Resource<WebGLBuffer>
+  extends Buffer
 {
-  @Nonnull
-  private final ArrayBufferView _data;
-  @UsageType
-  private final int _usage;
   @Nonnull
   private final Accessor _accessor;
 
@@ -30,57 +25,13 @@ public final class AttributeBuffer
                           @UsageType final int usage,
                           @Nonnull final Accessor accessor )
   {
-    super( gl, true );
-    _data = Objects.requireNonNull( data );
-    _usage = usage;
+    super( gl, data, usage, WebGL2RenderingContext.ARRAY_BUFFER );
     _accessor = Objects.requireNonNull( accessor );
-  }
-
-  @Nonnull
-  public ArrayBufferView getData()
-  {
-    return _data;
-  }
-
-  @UsageType
-  public int getUsage()
-  {
-    return _usage;
   }
 
   @Nonnull
   public Accessor getAccessor()
   {
     return _accessor;
-  }
-
-  @Override
-  public void bind()
-  {
-    gl().bindBuffer( WebGL2RenderingContext.ARRAY_BUFFER, getHandle() );
-  }
-
-  @Override
-  public void unbind()
-  {
-    gl().bindBuffer( WebGL2RenderingContext.ARRAY_BUFFER, null );
-  }
-
-  @Nonnull
-  @Override
-  protected WebGLBuffer allocateResource()
-  {
-    final WebGL2RenderingContext gl = gl();
-    final WebGLBuffer buffer = gl.createBuffer();
-    assert null != buffer;
-    gl.bindBuffer( WebGL2RenderingContext.ARRAY_BUFFER, buffer );
-    gl.bufferData( WebGL2RenderingContext.ARRAY_BUFFER, _data, _usage );
-    return buffer;
-  }
-
-  @Override
-  protected void releaseResource( @Nonnull final WebGLBuffer handle )
-  {
-    gl().deleteBuffer( handle );
   }
 }
--- src/main/java/org/realityforge/webgl/util/IndexBuffer.java
+++ src/main/java/org/realityforge/webgl/util/IndexBuffer.java
@@ -4,16 +4,11 @@ import elemental3.core.ArrayBufferView;
 import elemental3.gl.DrawElementDataType;
 import elemental3.gl.UsageType;
 import elemental3.gl.WebGL2RenderingContext;
-import elemental3.gl.WebGLBuffer;
 import javax.annotation.Nonnull;
 
 public final class IndexBuffer
-  extends Resource<WebGLBuffer>
+  extends Buffer
 {
-  @Nonnull
-  private final ArrayBufferView _data;
-  @UsageType
-  private final int _usage;
   @DrawElementDataType
   private final int _type;
 
@@ -29,57 +24,14 @@ public final class IndexBuffer
                       @UsageType final int usage,
                       @DrawElementDataType final int type )
   {
-    super( gl, true );
-    _data = data;
-    _usage = usage;
+    super( gl, data, usage,WebGL2RenderingContext.ELEMENT_ARRAY_BUFFER );
+    DrawElementDataType.Validator.assertValid( type );
     _type = type;
-  }
-
-  @Nonnull
-  public ArrayBufferView getData()
-  {
-    return _data;
-  }
-
-  @UsageType
-  public int getUsage()
-  {
-    return _usage;
   }
 
   @DrawElementDataType
   public int getType()
   {
     return _type;
-  }
-
-  @Override
-  public void bind()
-  {
-    gl().bindBuffer( WebGL2RenderingContext.ELEMENT_ARRAY_BUFFER, getHandle() );
-  }
-
-  @Override
-  public void unbind()
-  {
-    gl().bindBuffer( WebGL2RenderingContext.ELEMENT_ARRAY_BUFFER, null );
-  }
-
-  @Nonnull
-  @Override
-  protected WebGLBuffer allocateResource()
-  {
-    final WebGL2RenderingContext gl = gl();
-    final WebGLBuffer buffer = gl.createBuffer();
-    assert null != buffer;
-    gl.bindBuffer( WebGL2RenderingContext.ELEMENT_ARRAY_BUFFER, getHandle() );
-    gl.bufferData( WebGL2RenderingContext.ELEMENT_ARRAY_BUFFER, _data, _usage );
-    return buffer;
-  }
-
-  @Override
-  protected void releaseResource( @Nonnull final WebGLBuffer handle )
-  {
-    gl().deleteBuffer( handle );
   }
 }
