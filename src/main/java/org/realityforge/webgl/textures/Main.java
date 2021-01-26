02e2c2899fbeb2fe7fa27501c0542f97656b67a6
Peter Donald <peter@realityforge.org>
2021-01-26 17:55:55 +1100

    Consolidate example

--- src/main/java/org/realityforge/webgl/textures/Main.java
+++ src/main/java/org/realityforge/webgl/textures/Main.java
@@ -3,24 +3,224 @@ package org.realityforge.webgl.textures;
 import com.google.gwt.core.client.EntryPoint;
 import elemental3.Global;
 import elemental3.HTMLCanvasElement;
+import elemental3.core.Float32Array;
+import elemental3.gl.GLSL;
 import elemental3.gl.WebGL2RenderingContext;
+import elemental3.gl.WebGLProgram;
 import javax.annotation.Nonnull;
 import org.realityforge.vecmath.Matrix4d;
+import org.realityforge.webgl.util.Accessor;
+import org.realityforge.webgl.util.Attribute;
+import org.realityforge.webgl.util.AttributeBuffer;
 import org.realityforge.webgl.util.CanvasUtil;
+import org.realityforge.webgl.util.GL;
+import org.realityforge.webgl.util.Geometry2;
 import org.realityforge.webgl.util.MathUtil;
 
 public final class Main
   implements EntryPoint
 {
+  // Vertices position data
+  private static final double[] POSITIONS = new double[]
+    {
+      -0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, 0.5, -0.5,
+      0.5, 0.5, -0.5,
+      -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+
+      -0.5, -0.5, 0.5,
+      0.5, -0.5, 0.5,
+      0.5, 0.5, 0.5,
+      0.5, 0.5, 0.5,
+      -0.5, 0.5, 0.5,
+      -0.5, -0.5, 0.5,
+
+      -0.5, 0.5, 0.5,
+      -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+      -0.5, -0.5, -0.5,
+      -0.5, -0.5, 0.5,
+      -0.5, 0.5, 0.5,
+
+      0.5, 0.5, 0.5,
+      0.5, 0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, 0.5,
+      0.5, 0.5, 0.5,
+
+      -0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+      0.5, -0.5, 0.5,
+      0.5, -0.5, 0.5,
+      -0.5, -0.5, 0.5,
+      -0.5, -0.5, -0.5,
+
+      -0.5, 0.5, -0.5,
+      0.5, 0.5, -0.5,
+      0.5, 0.5, 0.5,
+      0.5, 0.5, 0.5,
+      -0.5, 0.5, 0.5,
+      -0.5, 0.5, -0.5
+    };
+  // Vertices color data in RGBA form
+  private static final double[] COLORS = new double[]
+    {
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+      1.0, 0.0, 0.0, 1.0, // Front face
+
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+      0.0, 1.0, 0.0, 1.0, // Back face
+
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+      0.0, 0.0, 1.0, 1.0, // Top face
+
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+      1.0, 1.0, 0.0, 1.0, // Bottom face
+
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+      1.0, 0.0, 1.0, 1.0, // Right face
+
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0, // Left face
+      0.0, 1.0, 1.0, 1.0 // Left face
+    };
+  private static final double[] TEXTURE_COORDINATES = new double[]
+    {
+      0.0, 1.0,
+      1.0, 1.0,
+      1.0, 0.0,
+      1.0, 0.0,
+      0.0, 0.0,
+      0.0, 1.0,
+
+      0.0, 1.0,
+      1.0, 1.0,
+      1.0, 0.0,
+      1.0, 0.0,
+      0.0, 0.0,
+      0.0, 1.0,
+
+      1.0, 1.0,
+      1.0, 0.0,
+      0.0, 0.0,
+      0.0, 0.0,
+      0.0, 1.0,
+      1.0, 1.0,
+
+      1.0, 1.0,
+      1.0, 0.0,
+      0.0, 0.0,
+      0.0, 0.0,
+      0.0, 1.0,
+      1.0, 1.0,
+
+      0.0, 0.0,
+      1.0, 0.0,
+      1.0, 1.0,
+      1.0, 1.0,
+      0.0, 1.0,
+      0.0, 0.0,
+
+      0.0, 0.0,
+      1.0, 0.0,
+      1.0, 1.0,
+      1.0, 1.0,
+      0.0, 1.0,
+      0.0, 0.0
+    };
+  // The vertex shader that will be run for every vertex
+  @GLSL
+  @Nonnull
+  private static final String VERTEX_SHADER_SOURCE =
+    // The shader language is OpenGL 3 (i.e. 300) ES and this version pragma must
+    // be the first thing present in the shader source
+    "#version 300 es\n" +
+    // The incoming vertex position
+    "in vec3 position;\n" +
+    // The incoming vertex color
+    "in vec4 color;\n" +
+    // The incoming texture coordinate
+    "in vec2 textureCoordinate;\n" +
+    // The output vertex color that will be fed to the next shader
+    "out vec4 fcolor;\n" +
+    // The outgoing texture coordinate
+    "out vec2 fTextureCoordinate;\n" +
+    "\n" +
+    // The following are the unions (aka constant across multiple vertices)
+    "uniform mat4 modelMatrix;\n" +
+    "uniform mat4 viewMatrix;\n" +
+    "uniform mat4 projectionMatrix;\n" +
+    // The main program/kernel
+    "void main()\n" +
+    "{\n" +
+    // Copy position from input to output, converting to vec4 by adding using 1 for 4th dimension
+    // and transforming via model/view/project matrices
+    "  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1);" +
+    // Copy color from input to output
+    "  fcolor = color;" +
+    // Copy textureCoordinate from input to output
+    "  fTextureCoordinate = textureCoordinate;" +
+    "}\n";
+  // The fragment shader that will be run for every pixel
+  @GLSL
+  private static final String FRAGMENT_SHADER_SOURCE =
+    // The version of language in use
+    "#version 300 es\n" +
+    // There is no default precision for floats in fragment shaders so specify it
+    "precision mediump float;\n" +
+    // The incoming fragment color
+    "in vec4 fcolor;\n" +
+    // The incoming fragment texture coordinate
+    "in vec2 fTextureCoordinate;\n" +
+    // The uniform for texture data
+    "uniform sampler2D textureData0;\n" +
+    // The uniform for other texture data
+    "uniform sampler2D textureData1;\n" +
+    // The output fragment color
+    "out vec4 finalColor;\n" +
+    "" +
+    // The main program/kernel
+    "void main()\n" +
+    "{\n" +
+    // Copy color from input to output
+    "  finalColor = mix( texture( textureData0, fTextureCoordinate ), texture( textureData1, fTextureCoordinate ), 0.5) * fcolor;" +
+    "}\n";
   @Nonnull
   private final Matrix4d _modelMatrix = new Matrix4d();
   @Nonnull
   private final Matrix4d _viewMatrix = new Matrix4d();
   @Nonnull
   private final Matrix4d _projectionMatrix = new Matrix4d();
-  private Mesh _mesh;
+  private Material _material;
   private double _angle;
   private boolean _sentToGpu;
+  private Geometry2 _geometry;
 
   @Override
   public void onModuleLoad()
@@ -30,7 +230,24 @@ public final class Main
 
     _projectionMatrix.setPerspective( MathUtil.degreesToRadians( 45 ), CanvasUtil.getAspect( canvas ), 0.1, 10.0 );
 
-    _mesh = CubeTemplate.create( gl );
+    _material = new Material( gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE );
+
+    final WebGLProgram program = _material.getProgram();
+    _geometry = new Geometry2( gl,
+                               36,
+                               new Attribute( new AttributeBuffer( gl,
+                                                                   new Float32Array( POSITIONS ),
+                                                                   new Accessor( 3 ) ),
+                                              GL.getAttribLocation( gl, program, "position" ) ),
+                               new Attribute( new AttributeBuffer( gl,
+                                                                   new Float32Array( COLORS ),
+                                                                   new Accessor( 4 ) ),
+                                              GL.getAttribLocation( gl, program, "color" ) ),
+                               new Attribute( new AttributeBuffer( gl,
+                                                                   new Float32Array( TEXTURE_COORDINATES ),
+                                                                   new Accessor( 2 ) ),
+                                              GL.getAttribLocation( gl, program, "textureCoordinate" ) ) );
+    _geometry.allocate();
 
     Global.requestAnimationFrame( t -> renderFrame( canvas, gl ) );
   }
@@ -39,14 +256,22 @@ public final class Main
   {
     Global.requestAnimationFrame( t -> renderFrame( canvas, gl ) );
     CanvasUtil.resize( gl, canvas );
-    if ( !_mesh.areTexturesLoaded() )
+    if ( null == _material.getTexture1() || null == _material.getTexture2() )
     {
       return;
     }
     else if ( !_sentToGpu )
     {
       // Have to send to GPU here as otherwise texture data has not loaded
-      _mesh.sendToGpu( gl );
+      gl.useProgram( _material.getProgram() );
+
+      gl.activeTexture( WebGL2RenderingContext.TEXTURE0 );
+      gl.bindTexture( WebGL2RenderingContext.TEXTURE_2D, _material.getTexture1() );
+      gl.uniform1i( _material.getTextureData0Location(), 0 );
+
+      gl.activeTexture( WebGL2RenderingContext.TEXTURE1 );
+      gl.bindTexture( WebGL2RenderingContext.TEXTURE_2D, _material.getTexture2() );
+      gl.uniform1i( _material.getTextureData1Location(), 1 );
       _sentToGpu = true;
     }
 
@@ -60,7 +285,14 @@ public final class Main
 
     _viewMatrix.setIdentity();
 
-    _mesh.render( gl, _modelMatrix, _viewMatrix, _projectionMatrix );
+    gl.useProgram( _material.getProgram() );
+    gl.uniformMatrix4fv( _material.getModelMatrixLocation(), false, new Float32Array( _modelMatrix.toArray() ) );
+    gl.uniformMatrix4fv( _material.getViewMatrixLocation(), false, new Float32Array( _viewMatrix.toArray() ) );
+    gl.uniformMatrix4fv( _material.getProjectionMatrixLocation(),
+                         false,
+                         new Float32Array( _projectionMatrix.toArray() ) );
+
+    _geometry.draw();
 
     _angle += 0.01;
   }
