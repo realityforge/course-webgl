e3401cf260a2e24c92fc49de09dd6be3fd613bde
Peter Donald <peter@realityforge.org>
2021-01-26 21:01:16 +1100

    Add support for normals

--- src/main/java/org/realityforge/webgl/util/geometries/CubeGeometryFactory.java
+++ src/main/java/org/realityforge/webgl/util/geometries/CubeGeometryFactory.java
@@ -17,6 +17,10 @@ import org.realityforge.webgl.util.Geometry2;
 public final class CubeGeometryFactory
 {
   /**
+   * If specified the factory should create vertex normals.
+   */
+  public static final int NORMALS = 0x01;
+  /**
    * If specified the factory should create vertex uvs.
    */
   public static final int UVS = 0x02;
@@ -27,6 +31,8 @@ public final class CubeGeometryFactory
   // We use JsArray of doubles as this will autogrow and contains native js numbers
   @Nonnull
   private final JsArray<Double> _positions = new JsArray<>();
+  @Nullable
+  private final JsArray<Double> _normals;
   @Nullable
   private final JsArray<Double> _uvs;
   @Nullable
@@ -44,7 +50,7 @@ public final class CubeGeometryFactory
   @Nonnull
   public static Geometry2 create( @Nonnull final WebGL2RenderingContext gl,
                                   final double length,
-                                  @MagicConstant( flags = { UVS, COLORS } ) final int options )
+                                  @MagicConstant( flags = { NORMALS, UVS, COLORS } ) final int options )
   {
     return new CubeGeometryFactory( gl, WebGL2RenderingContext.TRIANGLES, length, options )._geometry;
   }
@@ -52,10 +58,11 @@ public final class CubeGeometryFactory
   private CubeGeometryFactory( @Nonnull final WebGL2RenderingContext gl,
                                @DrawPrimitiveType final int mode,
                                final double length,
-                               @MagicConstant( flags = { UVS, COLORS } ) final int options )
+                               @MagicConstant( flags = { NORMALS, UVS, COLORS } ) final int options )
   {
     _uvs = UVS == ( UVS & options ) ? new JsArray<>() : null;
     _colors = COLORS == ( COLORS & options ) ? new JsArray<>() : null;
+    _normals = NORMALS == ( NORMALS & options ) ? new JsArray<>() : null;
 
     buildData( length );
 
@@ -68,6 +75,10 @@ public final class CubeGeometryFactory
     if ( null != _uvs )
     {
       attributes.add( new Attribute( new AttributeBuffer( gl, new Float32Array( _uvs ), new Accessor( 2 ) ) ) );
+    }
+    if ( null != _normals )
+    {
+      attributes.add( new Attribute( new AttributeBuffer( gl, new Float32Array( _normals ), new Accessor( 3 ) ) ) );
     }
     _geometry = new Geometry2( gl, mode, 0, 36, null, attributes.toArray( new Attribute[ 0 ] ) );
   }
@@ -232,6 +243,58 @@ public final class CubeGeometryFactory
       for ( double value : colors )
       {
         _colors.push( value );
+      }
+    }
+    if ( null != _normals )
+    {
+      final double[] normals = new double[]
+        {
+          // Front face
+          0.0, 0.0, -1.0,
+          0.0, 0.0, -1.0,
+          0.0, 0.0, -1.0,
+          0.0, 0.0, -1.0,
+          0.0, 0.0, -1.0,
+          0.0, 0.0, -1.0,
+
+          0.0, 0.0, 1.0,
+          0.0, 0.0, 1.0,
+          0.0, 0.0, 1.0,
+          0.0, 0.0, 1.0,
+          0.0, 0.0, 1.0,
+          0.0, 0.0, 1.0,
+
+          -1.0, 0.0, 0.0,
+          -1.0, 0.0, 0.0,
+          -1.0, 0.0, 0.0,
+          -1.0, 0.0, 0.0,
+          -1.0, 0.0, 0.0,
+          -1.0, 0.0, 0.0,
+
+          1.0, 0.0, 0.0,
+          1.0, 0.0, 0.0,
+          1.0, 0.0, 0.0,
+          1.0, 0.0, 0.0,
+          1.0, 0.0, 0.0,
+          1.0, 0.0, 0.0,
+
+          0.0, -1.0, 0.0,
+          0.0, -1.0, 0.0,
+          0.0, -1.0, 0.0,
+          0.0, -1.0, 0.0,
+          0.0, -1.0, 0.0,
+          0.0, -1.0, 0.0,
+
+          0.0, 1.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 1.0, 0.0
+        };
+      for ( double value : normals )
+      {
+        _normals.push( value );
       }
     }
   }
