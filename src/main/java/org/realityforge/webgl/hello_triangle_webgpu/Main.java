73dd8c62e337f880a13fee25d7d5db66e48f681f
Peter Donald <peter@realityforge.org>
2021-06-30 20:44:53 +1000

    Add a simple WebGPU example

---
+++ src/main/java/org/realityforge/webgl/hello_triangle_webgpu/Main.java
@@ -0,0 +1,115 @@
+package org.realityforge.webgl.hello_triangle_webgpu;
+
+import akasha.HTMLCanvasElement;
+import akasha.RenderContextType;
+import akasha.RenderingContext;
+import akasha.WindowGlobal;
+import akasha.gpu.GPUAdapter;
+import akasha.gpu.GPUCanvasContext;
+import akasha.gpu.GPUColorDict;
+import akasha.gpu.GPUColorTargetState;
+import akasha.gpu.GPUCommandBuffer;
+import akasha.gpu.GPUCommandEncoder;
+import akasha.gpu.GPUDevice;
+import akasha.gpu.GPUFragmentState;
+import akasha.gpu.GPUPrimitiveState;
+import akasha.gpu.GPUPrimitiveTopology;
+import akasha.gpu.GPURenderPassColorAttachment;
+import akasha.gpu.GPURenderPassDescriptor;
+import akasha.gpu.GPURenderPassEncoder;
+import akasha.gpu.GPURenderPipeline;
+import akasha.gpu.GPURenderPipelineDescriptor;
+import akasha.gpu.GPUShaderModuleDescriptor;
+import akasha.gpu.GPUStoreOp;
+import akasha.gpu.GPUSwapChain;
+import akasha.gpu.GPUSwapChainDescriptor;
+import akasha.gpu.GPUTextureFormat;
+import akasha.gpu.GPUTextureView;
+import akasha.gpu.GPUVertexState;
+import akasha.gpu.WGSL;
+import com.google.gwt.core.client.EntryPoint;
+import javax.annotation.Nonnull;
+import jsinterop.base.Js;
+import org.realityforge.webgl.util.CanvasUtil;
+
+@SuppressWarnings( "UnstableApiUsage" )
+public final class Main
+  implements EntryPoint
+{
+  private GPUDevice _device;
+  private GPURenderPipeline _pipeline;
+  private GPUSwapChain _swapChain;
+
+  @Override
+  public void onModuleLoad()
+  {
+    WindowGlobal.navigator().gpu().requestAdapter().then( GPUAdapter::requestDevice ).thenAccept( this::onStart );
+
+  }
+
+  private void onStart( @Nonnull final GPUDevice device )
+  {
+    _device = device;
+    final HTMLCanvasElement canvas = CanvasUtil.createCanvas();
+
+    final RenderingContext context = canvas.getContext( RenderContextType.gpupresent );
+    assert null != context;
+    final GPUCanvasContext gl = Js.uncheckedCast( context );
+    _swapChain = gl.configureSwapChain( GPUSwapChainDescriptor.create( _device, GPUTextureFormat.bgra8unorm ) );
+
+    @WGSL
+    final String vertexShader =
+      "[[stage(vertex)]]\n" +
+      "fn main([[builtin(vertex_index)]] VertexIndex : u32)\n" +
+      "     -> [[builtin(position)]] vec4<f32> {\n" +
+      "  var pos = array<vec2<f32>, 3>(\n" +
+      "      vec2<f32>(0.0, 0.5),\n" +
+      "      vec2<f32>(-0.5, -0.5),\n" +
+      "      vec2<f32>(0.5, -0.5));\n" +
+      "\n" +
+      "  return vec4<f32>(pos[VertexIndex], 0.0, 1.0);\n" +
+      "}";
+    final GPUVertexState.Builder vertexState =
+      GPUVertexState.create( "main",
+                             _device.createShaderModule( GPUShaderModuleDescriptor.create( vertexShader ) ) );
+    @WGSL
+    final String fragmentShader =
+      "[[stage(fragment)]]\n" +
+      "fn main() -> [[location(0)]] vec4<f32> {\n" +
+      "  return vec4<f32>(1.0, 0.0, 0.0, 1.0);\n" +
+      "}";
+    final GPUFragmentState fragmentState =
+      GPUFragmentState.create( "main",
+                               _device.createShaderModule( GPUShaderModuleDescriptor.create( fragmentShader ) ),
+                               new GPUColorTargetState[]{GPUColorTargetState.create( GPUTextureFormat.bgra8unorm )} );
+
+    _pipeline = _device.createRenderPipeline( GPURenderPipelineDescriptor
+                                                .create( vertexState )
+                                                .fragment( fragmentState )
+                                                .primitive( GPUPrimitiveState
+                                                              .create()
+                                                              .topology( GPUPrimitiveTopology.triangle_list ) ) );
+
+    WindowGlobal.requestAnimationFrame( t -> renderFrame() );
+  }
+
+  private void renderFrame()
+  {
+    WindowGlobal.requestAnimationFrame( t -> renderFrame() );
+    final GPUCommandEncoder commandEncoder = _device.createCommandEncoder();
+    final GPUTextureView textureView = _swapChain.getCurrentTexture().createView();
+
+    final GPURenderPassColorAttachment attachment =
+      GPURenderPassColorAttachment.create( GPUColorDict.create( 1, 0, 0, 0 ),
+                                           GPUStoreOp.store,
+                                           textureView );
+
+    final GPURenderPassEncoder passEncoder =
+      commandEncoder.beginRenderPass( GPURenderPassDescriptor.create( new GPURenderPassColorAttachment[]{ attachment } ) );
+    passEncoder.setPipeline( _pipeline );
+    passEncoder.draw( 3, 1, 0, 0 );
+    passEncoder.endPass();
+
+    _device.queue().submit( new GPUCommandBuffer[]{ commandEncoder.finish() } );
+  }
+}
