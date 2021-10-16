f7f619544e6a7afd498e742add6995a7507eabbd
Peter Donald <peter@realityforge.org>
2021-10-17 08:47:30 +1100

    Initial pass at rendering dragon

---
+++ src/main/java/org/realityforge/webgl/webgpu/dragon/Main.java
@@ -0,0 +1,257 @@
+package org.realityforge.webgl.webgpu.dragon;
+
+import akasha.HTMLCanvasElement;
+import akasha.WindowGlobal;
+import akasha.core.Float32Array;
+import akasha.core.Uint32Array;
+import akasha.gpu.GPUAdapter;
+import akasha.gpu.GPUBindGroup;
+import akasha.gpu.GPUBindGroupDescriptor;
+import akasha.gpu.GPUBindGroupEntry;
+import akasha.gpu.GPUBuffer;
+import akasha.gpu.GPUBufferBinding;
+import akasha.gpu.GPUBufferDescriptor;
+import akasha.gpu.GPUBufferUsage;
+import akasha.gpu.GPUCanvasConfiguration;
+import akasha.gpu.GPUCanvasContext;
+import akasha.gpu.GPUColorDict;
+import akasha.gpu.GPUColorTargetState;
+import akasha.gpu.GPUCommandEncoder;
+import akasha.gpu.GPUCompareFunction;
+import akasha.gpu.GPUCullMode;
+import akasha.gpu.GPUDepthStencilState;
+import akasha.gpu.GPUDevice;
+import akasha.gpu.GPUExtent3DDict;
+import akasha.gpu.GPUFragmentState;
+import akasha.gpu.GPUPrimitiveState;
+import akasha.gpu.GPUPrimitiveTopology;
+import akasha.gpu.GPURenderPassColorAttachment;
+import akasha.gpu.GPURenderPassDepthStencilAttachment;
+import akasha.gpu.GPURenderPassDescriptor;
+import akasha.gpu.GPURenderPassEncoder;
+import akasha.gpu.GPURenderPipeline;
+import akasha.gpu.GPURenderPipelineDescriptor;
+import akasha.gpu.GPUShaderModuleDescriptor;
+import akasha.gpu.GPUStoreOp;
+import akasha.gpu.GPUTexture;
+import akasha.gpu.GPUTextureDescriptor;
+import akasha.gpu.GPUTextureFormat;
+import akasha.gpu.GPUTextureUsage;
+import akasha.gpu.GPUTextureView;
+import akasha.gpu.GPUVertexAttribute;
+import akasha.gpu.GPUVertexBufferLayout;
+import akasha.gpu.GPUVertexFormat;
+import akasha.gpu.GPUVertexState;
+import akasha.gpu.WGSL;
+import com.google.gwt.core.client.EntryPoint;
+import javax.annotation.Nonnull;
+import org.realityforge.vecmath.Matrix4d;
+import org.realityforge.vecmath.Vector3f;
+import org.realityforge.webgl.util.CanvasUtil;
+import org.realityforge.webgl.util.MathUtil;
+import org.realityforge.webgl.webgpu.util.WebGpuKit;
+import org.realityforge.webgl.webgpu.util.dragon.Asset;
+
+@SuppressWarnings( "UnstableApiUsage" )
+public final class Main
+  implements EntryPoint
+{
+  private GPUDevice _device;
+  private GPURenderPipeline _pipeline;
+  private GPUCanvasContext _gl;
+  private GPUAdapter _adapter;
+  private GPUBindGroup _uniformBindGroup;
+  private GPUBuffer _verticesBuffer;
+  private GPUBuffer _indexBuffer;
+  private GPURenderPassDescriptor.Builder _renderPassDescriptor;
+  private final Matrix4d _projectionMatrix = new Matrix4d();
+  private GPUBuffer _uniformBuffer;
+  private Asset _asset;
+
+  @Override
+  public void onModuleLoad()
+  {
+    Asset.loadStanfordDragon().thenAccept( data -> {
+      _asset = data;
+      WindowGlobal.navigator().gpu().requestAdapter().then( adapter -> {
+        _adapter = adapter;
+        return adapter.requestDevice();
+      } ).thenAccept( this::onStart );
+    } );
+  }
+
+  private void onStart( @Nonnull final GPUDevice device )
+  {
+    _device = device;
+    final HTMLCanvasElement canvas = WebGpuKit.createFullWindowCanvas();
+
+    _gl = WebGpuKit.getGpuCanvasContext( canvas );
+
+    // Use the preferred format of adapter instead of hardcoding to a specific format ala bgra8unorm.
+    @GPUTextureFormat
+    final String textureFormat = _gl.getPreferredFormat( _adapter );
+
+    final GPUExtent3DDict presentationSize = WebGpuKit.calcGpuExtent3D( canvas );
+    _gl.configure( GPUCanvasConfiguration
+                     .create( _device, textureFormat )
+                     //Ensure the configured size takes into account the device pixel ratio.
+                     .size( presentationSize ) );
+
+    final double[] vertices = _asset.getVertices();
+    _verticesBuffer = device.createBuffer( GPUBufferDescriptor
+                                             .create( vertices.length * Float32Array.BYTES_PER_ELEMENT,
+                                                      GPUBufferUsage.VERTEX )
+                                             .mappedAtCreation( true ) );
+    new Float32Array( _verticesBuffer.getMappedRange() ).set( vertices );
+    _verticesBuffer.unmap();
+
+    final double[] indices = _asset.getIndices();
+    _indexBuffer = device.createBuffer( GPUBufferDescriptor
+                                          .create( _asset.getIndexByteCount(), GPUBufferUsage.INDEX )
+                                          .mappedAtCreation( true ) );
+    new Uint32Array( _indexBuffer.getMappedRange() ).set( indices );
+    _indexBuffer.unmap();
+
+    @WGSL
+    final String vertexShader =
+      "[[block]] struct Uniforms {\n" +
+      "  modelViewProjectionMatrix : mat4x4<f32>;\n" +
+      "};\n" +
+      "[[binding(0), group(0)]] var<uniform> uniforms : Uniforms;\n" +
+      "\n" +
+      "struct VertexOutput {\n" +
+      "  [[builtin(position)]] Position : vec4<f32>;\n" +
+      "  [[location(0)]] fragUV : vec2<f32>;\n" +
+      "  [[location(1)]] fragPosition: vec4<f32>;\n" +
+      "};\n" +
+      "\n" +
+      "[[stage(vertex)]]\n" +
+      "fn main([[location(0)]] position : vec4<f32>,\n" +
+      "        [[location(1)]] uv : vec2<f32>) -> VertexOutput {\n" +
+      "  var output : VertexOutput;\n" +
+      "  output.Position = uniforms.modelViewProjectionMatrix * position;\n" +
+      "  output.fragUV = uv;\n" +
+      "  output.fragPosition = 0.5 * (position + vec4<f32>(1.0, 1.0, 1.0, 1.0));\n" +
+      "  return output;\n" +
+      "}\n";
+    final GPUVertexState.Builder vertexState =
+      GPUVertexState
+        .create( _device.createShaderModule( GPUShaderModuleDescriptor.code( vertexShader ) ), "main" )
+        .buffers( GPUVertexBufferLayout.create( _asset.vertexSize(),
+                                                // position
+                                                GPUVertexAttribute.create( GPUVertexFormat.float32x3,
+                                                                           0,
+                                                                           0 ),
+                                                // uv
+                                                GPUVertexAttribute.create( GPUVertexFormat.float32x2,
+                                                                           Vector3f.BYTES * 2,
+                                                                           1 ) ) );
+
+    @WGSL
+    final String fragmentShader =
+      "[[stage(fragment)]]\n" +
+      "fn main([[location(0)]] fragUV: vec2<f32>,\n" +
+      "        [[location(1)]] fragPosition: vec4<f32>) -> [[location(0)]] vec4<f32> {\n" +
+      "  return fragPosition;\n" +
+      "}\n";
+    final GPUFragmentState fragmentState =
+      GPUFragmentState.create( _device.createShaderModule( GPUShaderModuleDescriptor.code( fragmentShader ) ),
+                               "main",
+                               GPUColorTargetState.format( textureFormat ) );
+
+    _pipeline =
+      _device.createRenderPipeline( GPURenderPipelineDescriptor
+                                      .vertex( vertexState )
+                                      .fragment( fragmentState )
+                                      .primitive( GPUPrimitiveState
+                                                    .create()
+                                                    .topology( GPUPrimitiveTopology.triangle_list )
+                                                    .cullMode( GPUCullMode.back ) )
+                                      // Enable depth testing so that the fragment closest to the camera
+                                      // is rendered in front.
+                                      .depthStencil( GPUDepthStencilState
+                                                       .format( GPUTextureFormat.depth24plus )
+                                                       .depthCompare( GPUCompareFunction.less )
+                                                       .depthWriteEnabled( true ) ) );
+
+    final GPUTexture depthTexture =
+      device.createTexture( GPUTextureDescriptor.create( presentationSize,
+                                                         GPUTextureFormat.depth24plus,
+                                                         GPUTextureUsage.RENDER_ATTACHMENT ) );
+
+    final int uniformBufferSize = Matrix4d.FLOAT_BYTES;
+    _uniformBuffer =
+      device.createBuffer( GPUBufferDescriptor.create( uniformBufferSize,
+                                                       GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST ) );
+
+    final GPUBindGroupDescriptor.Builder bindGroupDescriptor =
+      GPUBindGroupDescriptor.create( _pipeline.getBindGroupLayout( 0 ),
+                                     GPUBindGroupEntry.create( 0, GPUBufferBinding.buffer( _uniformBuffer ) ) );
+    _uniformBindGroup = device.createBindGroup( bindGroupDescriptor );
+
+    final GPUTextureView textureView = _gl.getCurrentTexture().createView();
+
+    final GPURenderPassColorAttachment attachment =
+      GPURenderPassColorAttachment.create( textureView,
+                                           GPUColorDict.create( 0.5, 0.5, 0.5, 1 ),
+                                           GPUStoreOp.store );
+
+    _renderPassDescriptor =
+      GPURenderPassDescriptor
+        .colorAttachments( attachment )
+        .depthStencilAttachment( GPURenderPassDepthStencilAttachment.create( depthTexture.createView(),
+                                                                             1.0F,
+                                                                             GPUStoreOp.store,
+                                                                             0,
+                                                                             GPUStoreOp.store ) );
+
+    _projectionMatrix.setPerspective( MathUtil.degreesToRadians( 72 ),
+                                      CanvasUtil.getAspect( canvas ),
+                                      1,
+                                      100 );
+
+    WindowGlobal.requestAnimationFrame( t -> renderFrame() );
+  }
+
+  @Nonnull
+  private Float32Array getTransformationMatrix()
+  {
+    final Matrix4d viewMatrix = new Matrix4d();
+    viewMatrix.setIdentity();
+    viewMatrix.translate( 0, 0, -10 );
+    viewMatrix.rotateX( System.currentTimeMillis() / 1000.0 );
+    viewMatrix.rotateY( System.currentTimeMillis() / 1000.0 );
+    //mat4.rotate( viewMatrix, viewMatrix, 1, vec3.fromValues(Math.sin(now), Math.cos(now), 0) );
+
+    final Matrix4d modelViewProjectionMatrix = new Matrix4d();
+    Matrix4d.multiply( modelViewProjectionMatrix, viewMatrix, _projectionMatrix );
+
+    return new Float32Array( modelViewProjectionMatrix.toArray() );
+  }
+
+  private void renderFrame()
+  {
+    WindowGlobal.requestAnimationFrame( t -> renderFrame() );
+
+    final Float32Array transformationMatrix = getTransformationMatrix();
+    _device.queue().writeBuffer( _uniformBuffer,
+                                 0,
+                                 transformationMatrix.buffer(),
+                                 transformationMatrix.byteOffset(),
+                                 transformationMatrix.byteLength() );
+
+    _renderPassDescriptor.colorAttachments().getAt( 0 ).setView( _gl.getCurrentTexture().createView() );
+
+    final GPUCommandEncoder commandEncoder = _device.createCommandEncoder();
+    final GPURenderPassEncoder passEncoder = commandEncoder.beginRenderPass( _renderPassDescriptor );
+
+    passEncoder.setPipeline( _pipeline );
+    passEncoder.setBindGroup( 0, _uniformBindGroup );
+    passEncoder.setVertexBuffer( 0, _verticesBuffer );
+    passEncoder.setIndexBuffer( _indexBuffer, _asset.indexFormat() );
+    passEncoder.drawIndexed( _asset.getIndices().length );
+    passEncoder.endPass();
+
+    _device.queue().submit( commandEncoder.finish() );
+  }
+}
---
+++ src/main/java/org/realityforge/webgl/webgpu/util/dragon/Asset.java
@@ -0,0 +1,83 @@
+package org.realityforge.webgl.webgpu.util.dragon;
+
+import akasha.Response;
+import akasha.WindowGlobal;
+import akasha.core.Uint32Array;
+import akasha.gpu.GPUIndexFormat;
+import akasha.promise.Promise;
+import javax.annotation.Nonnull;
+import jsinterop.annotations.JsOverlay;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@SuppressWarnings( { "unused", "UnstableApiUsage" } )
+@JsType( isNative = true, name = "Object", namespace = JsPackage.GLOBAL )
+public class Asset
+{
+  private String name;
+  //Vertices are laid out as:
+  // - 3 floats for position vertex,
+  // - 3 floats for vertex normal,
+  // - 2 floats for uv coordinates
+  private double[] vertices;
+  private double[] indices;
+
+  @JsOverlay
+  public static Promise<Asset> loadStanfordDragon()
+  {
+    return WindowGlobal
+      .fetch( "assets/dragon.json" )
+      .then( Response::json )
+      .then( data -> Promise.resolve( data.<Asset>cast() ) );
+  }
+
+  @JsOverlay
+  public final String getName()
+  {
+    return name;
+  }
+
+  @JsOverlay
+  public final int components()
+  {
+    return 3 + 3 + 2;
+  }
+
+  @JsOverlay
+  public final int vertexSize()
+  {
+    return Float.BYTES * components();
+  }
+
+  @JsOverlay
+  public final int vertexCount()
+  {
+    return vertices.length / components();
+  }
+
+  @JsOverlay
+  public final double[] getVertices()
+  {
+    return vertices;
+  }
+
+  @JsOverlay
+  public final double[] getIndices()
+  {
+    return indices;
+  }
+
+  @JsOverlay
+  @GPUIndexFormat
+  @Nonnull
+  public final String indexFormat()
+  {
+    return GPUIndexFormat.uint32;
+  }
+
+  @JsOverlay
+  public final int getIndexByteCount()
+  {
+    return indices.length * Uint32Array.BYTES_PER_ELEMENT;
+  }
+}
